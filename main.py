3bd27eea19d40f61a7c28c4123f02a32c4977ccf
Ninad Shelke <ninad.shelke@provakil.com>
2023-10-17 17:56:35 +0530

    third commit again

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 test = 'This is a git tutorial'
 print(' This file is meant to =', test)
 
-change = "this is the first change in the code"
-print('Changes made in the code = ', change)
+change = "The code got changed again"
+print('New changes made after second commit = ', change)
 
 print('This is my third commit')
