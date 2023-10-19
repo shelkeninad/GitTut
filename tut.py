286a3f2c45da4a0f8dc73aafd7cfc9c63182e97d
Ninad Shelke <ninad.shelke@provakil.com>
2023-10-19 19:56:53 +0530

    added SSH keys

--- tut.py
+++ tut.py
@@ -3,3 +3,6 @@
 print ('we have changed the amended commit again')
 print ('now we amend this commit in local branch')
 print ('but pusing this amending comit to origin will cause problems because it needs to overwrite the previous commit with this new commit')
+
+
+print ('shh keys added for better updation of code to github')
